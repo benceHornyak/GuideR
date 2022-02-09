f89ec326e7ee159f1df9830b278314fc7313af5b
Bence Hornyák <sir.bence15@gmail.com>
2022-02-09 22:04:15 +0100

    feat(data-access-appwrite): provide appwrite service

--- libs/data-access-appwrite/src/lib/data-access-appwrite.module.ts
+++ libs/data-access-appwrite/src/lib/data-access-appwrite.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppwriteService } from './appwrite-service.service';
 
 @NgModule({
   imports: [CommonModule],
+  providers: [AppwriteService],
 })
 export class DataAccessAppwriteModule {}
